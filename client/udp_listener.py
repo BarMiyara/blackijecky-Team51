0c6423fdf135b2476d43bbad201c42653509212d
Yuval pariente <parienyu@post.bgu.ac.il>
2026-01-17 20:07:57 +0200

    add UDP offer listener for client

---
+++ client/udp_listener.py
@@ -0,0 +1,23 @@
+import socket
+from common.protocol import unpack_offer, UDP_PORT
+
+def listen_for_offer():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    except Exception:
+        pass
+
+    sock.bind(('', UDP_PORT))
+    print("Client started, listening for offer requests...")
+
+    while True:
+        data, addr = sock.recvfrom(1024)
+        try:
+            server_name, tcp_port = unpack_offer(data)
+            server_ip = addr[0]
+            print(f"Received offer from {server_ip}")
+            sock.close()
+            return server_ip, tcp_port
+        except Exception:
+            continue
